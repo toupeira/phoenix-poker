0101354c52a190d55ef59386f51f8473cface6de
Markus Koller <markus-koller@gmx.ch>
2016-03-26 01:07:11 +0100

    Add basic view and PickCard action

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -1,54 +1,70 @@
-import Html exposing (Html, div, button, text)
+import String
 import StartApp.Simple as StartApp
 
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
 main =
-  StartApp.start {
-    model = initialModel,
-    view = view,
-    update = update
-  }
+  StartApp.start
+    { model = initialTable
+    , view = view
+    , update = update }
+
 
 -- MODEL
 
-initialModel = {
-    cards = [ 1, 2, 3, 5, 8 ],
-    players = [],
-    rounds = [],
-    deck = "default"
+initialTable =
+  { name = "Untitled"
+  , deck = "default"
+  , cards = [ 1.0, 2.0, 3.0, 5.0, 8.0 ]
+  , players = []
+  , currentRound = { picks = [], points = 0.0 }
+  , previousRounds =
+      [ { picks = [ 2.0, 3.0, 5.0 ], points = 3.0 } ]
   }
 
 type alias Model = Table
 
-type alias Table = {
-  cards: List Card,
-  players: List Player,
-  rounds: List Round,
-  deck: String
+type alias Table =
+  { name: String
+  , deck: String
+  , cards: List Card
+  , players: List Player
+  , currentRound: Round
+  , previousRounds: List Round
 }
 
-type alias Round = {
-  picks: List CardPick,
-  points: Points
+type alias Round =
+  { picks: List CardPick
+  , points: Points
 }
 
-type alias Player = {
-  name: String
-}
+type alias Player =
+  { name: String }
 
 type alias CardPick = Card
 type alias Card = Points
-type alias Points = Int
+type alias Points = Float
 
 -- UPDATE
 
-type Action = PickCard | StartRound | EndRound | ClearTable
+type Action
+  = PickCard Card
+  | StartRound
+  | EndRound
+  | ClearTable
 
-update : Action -> Model -> Model
+-- update : Action -> Model -> Model
 update action model =
   case action of
-    PickCard ->
-      -- add card to current round's list of picks
-      model
+    PickCard card ->
+      -- TODO: check for existing pick
+      -- TODO: replace existing pick
+      let round = model.currentRound
+          newRound = { round | picks = round.picks ++ [ card ] }
+      in
+        { model | currentRound = newRound }
     StartRound ->
       -- add new round
       model
@@ -56,10 +72,74 @@ update action model =
       -- set round points from card picks
       model
     ClearTable ->
-      initialModel
+      initialTable
+
 
 -- VIEW
 
-view : Signal.Address Action -> Model -> Html
+-- view : Signal.Address Action -> Model -> Html
 view address model =
-  text "hi there"
+  let previousRounds = List.map (roundView address) model.previousRounds
+      cards = List.map (cardView address) model.cards
+  in
+    div [ class "ui container" ]
+      [ h2 [ class "ui center aligned icon header" ]
+        [ i [ class "circular users icon" ] []
+        , text "Planning Poker" ]
+      , node "link" [ rel "stylesheet", href "phoenix-poker.css" ] []
+      , node "link" [ rel "stylesheet", href "//oss.maxcdn.com/semantic-ui/2.1.8/semantic.min.css" ] []
+      , node "script" [ src "//oss.maxcdn.com/semantic-ui/2.1.8/semantic.min.js" ] []
+      , node "script" [ src "//code.jquery.com/jquery-2.2.2.min.js" ] []
+      , div [ class "ui five column grid" ] cards
+      , table [ class "ui celled table" ]
+        [ thead []
+          [ tr []
+            [ th [] [ text "Picks" ]
+            , th [] [ text "Result" ]
+            ]
+          ]
+        , tbody [] [ roundView address model.currentRound ]
+        , tbody [] previousRounds
+        ]
+      ]
+
+roundView : Signal.Address Action -> Round -> Html
+roundView address round =
+  tr []
+    [ td [] [ text (String.join ", " (List.map toString round.picks)) ]
+    , td [] [ text (toString (pickAverage round.picks)) ]
+    ]
+
+
+cardView : Signal.Address Action -> Card -> Html
+cardView address card =
+  div [ class "column" ]
+    [ a [ class ("ui fluid card card-" ++ (toString card)), onClick address (PickCard card) ]
+      [ div [ class "content" ]
+        [ span [ class "header" ]
+          [ text ((toString card) ++ " " ++ (pluralize card "point" "points")) ]
+        ]
+      ]
+    ]
+
+
+pickAverage : List Card -> Int
+pickAverage picks =
+  case picks of
+    [] ->
+      0
+    _ ->
+      let sum = List.sum picks
+          length = toFloat (List.length picks)
+      in
+          round (sum / length)
+
+
+-- HELPERS
+
+pluralize : number -> String -> String -> String
+pluralize number singular plural =
+  if number == 1 then
+    singular
+  else
+    plural
