264565dd8faa3d8d824704af54772be4cb0f5657
Markus Koller <markus-koller@gmx.ch>
2016-03-27 12:07:34 +0200

    Get vendor assets working, update Brunch

--- brunch-config.js
+++ brunch-config.js
@@ -3,27 +3,27 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: {
-        "js/app.js": [
-          /^web\/static\/js\/app.js/,
+        'js/app.js': [
+          'web/static/js/app.js',
           /^web\/static\/vendor\/elm\//
         ],
-        "js/vendor.js": [
+        'js/vendor.js': [
           /^(?!web\/static\/(js\/app\.js|vendor\/elm\/))/
         ]
       }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: 'css/app.css'
     },
     templates: {
-      joinTo: "js/app.js"
+      joinTo: 'js/app.js'
     }
   },
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/web/static/assets". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
+    // By default, we set this to '/web/static/assets'. Files in this directory
+    // will be copied to `paths.public`, which is 'priv/static' by default.
     assets: /^(web\/static\/assets)/,
 
     ignored: [
@@ -37,13 +37,13 @@ exports.config = {
   paths: {
     // Dependencies and current project directories to watch
     watched: [
-      "web/static",
-      "web/elm",
-      "test/static"
+      'web/static',
+      'web/elm',
+      'test/static'
     ],
 
     // Where to compile files to
-    public: "priv/static"
+    public: 'priv/static'
   },
 
   // Configure your plugins
@@ -62,22 +62,25 @@ exports.config = {
       ignore: [/web\/static\/vendor/]
     },
     elmBrunch: {
-      elmFolder: "web/elm",
-      mainModules: [ "PhoenixPoker.elm" ],
-      outputFolder: "../static/vendor/elm"
+      elmFolder: 'web/elm',
+      mainModules: [ 'PhoenixPoker.elm' ],
+      outputFolder: '../static/vendor/elm'
     }
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["web/static/js/app"]
+      'js/app.js': ['web/static/js/app']
     }
   },
 
   npm: {
     enabled: true,
-    // Whitelist the npm deps to be pulled in as front-end assets.
-    // All other deps in package.json will be excluded from the bundle.
-    whitelist: ["phoenix", "phoenix_html"]
+    globals: {
+      jQuery: 'jquery'
+    },
+    static: [
+      'node_modules/phoenix_html/priv/static/phoenix_html.js'
+    ]
   }
 };
